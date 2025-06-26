1e76ccba2999e4959b74a379ecb51b7e5674a2a6
LifeNotSweet <sgazhev_jr@mail.ru>
2025-06-26 13:34:59 +0300

    Books struct

--- LibraryService/obj/Debug/net8.0/LibraryService.AssemblyInfo.cs
+++ LibraryService/obj/Debug/net8.0/LibraryService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0908519719188790f75c43266864ea867a075879")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d446d00862054e074ca0db64ed75798cb4d4394f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
